ce82b658bbf9f7d118fd5db6e251adb626ce2147
SumeetCodeJC <code.sumeet9@gmail.com>
2024-08-23 17:35:36 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+import AlbumSongsPage from "./components/Album_Songs_Page/AlbumSongsPage";
+import HomePage from "./components/Homepage/Homepage";
+
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/album/:title" element={<AlbumSongsPage />} />
+    </Routes>
   );
-}
+};
 
 export default App;
